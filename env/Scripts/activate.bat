2447b0adaa647ed42d2556d3fdd17a8fd3bc6565
Emnauel Rico Conde <ricocondee@gmail.com>
2021-10-23 14:34:31 -0500

    env

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Navarro92\Documents\GitHub\Sprint3-hotelProyect\env
+set VIRTUAL_ENV=C:\Users\ricoc\Desktop\Sprint3-hotelProyect\env
 
 if not defined PROMPT set PROMPT=$P$G
 
